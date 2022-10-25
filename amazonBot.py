85e0f4e0c415e7cbe8adbbda6b76ff575cb542e9
ishimoto <shamozi212@yahoo.co.jp>
2022-10-26 03:33:37 +0900

    test for amazon japan

--- amazonBot.py
+++ amazonBot.py
@@ -1,7 +1,9 @@
+from http.client import PAYMENT_REQUIRED
 import os
 import sys
 import time
 from random import randint
+import re
 
 from dotenv import load_dotenv
 from logger import logger as l
@@ -9,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from amazoncaptcha import AmazonCaptcha
+import pdb
 
 load_dotenv(verbose=True)
 dotenv_path = '.env'
@@ -19,9 +22,12 @@ LOGIN_PASSWORD = os.environ.get('LOGIN_PASSWORD', "")
 
 ITEM_URL = os.environ.get('ITEM_URL', "")
 
-CHECKOUT_URL = "https://www.amazon.com/gp/cart/view.html?ref_=nav_cart"
+CHECKOUT_URL = "https://www.amazon.co.jp/gp/cart/view.html?ref_=nav_cart"
+PAYMENT_URL = "https://www.amazon.co.jp/gp/buy/payselect/handlers/display.html?_from=cheetah"
+SUBMIT_URL = "https://www.amazon.co.jp/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1"
+
 ACCEPT_SHOP = "amazon"
-LIMIT_VALUE = int(os.environ.get('LIMIT_VALUE', "")) # Maximum USD for the purchase
+LIMIT_VALUE = int(os.environ.get('LIMIT_VALUE', "")) # Maximum Yen for the purchase
 
 # class GracefulKiller:
 #   kill_now = False
@@ -53,23 +59,31 @@ def validate_captcha(chromeDriver):
 
 
 def purchase_item(chromeDriver):
+    # Logs in and purchases the item
+    login(chromeDriver)
+    
     l.info("Starting purchase process...")
     # Checks if out of stock, price is right, and shipper is amazon keeps waiting if not in stock (for a random amount of time as to not get banned)
-    while not in_stock_check(chromeDriver) or not verify_price_within_limit(chromeDriver) or not seller_check(
-            chromeDriver):
+    while not in_stock_check(chromeDriver) or not verify_price_within_limit(chromeDriver): # or not seller_check(chromeDriver):
         l.info("Could not purchase desired item...waiting for stock/pricing/seller")
         sleep = randint(15, 90)
         l.info(f"Sleeping for {sleep} seconds...")
         time.sleep(sleep)
         chromeDriver.refresh()
 
-    # Logs in and purchases the item
-    login(chromeDriver)
+    # # Logs in and purchases the item
+    # login(chromeDriver)
 
     # Solve Captcha
-    validate_captcha(chromeDriver)
+    # validate_captcha(chromeDriver)
 
     if not checkout(chromeDriver):
+        return False
+    time.sleep(1)
+    if not go_to_order(chromeDriver):
+        return False
+    time.sleep(1)
+    if not submit_order(chromeDriver):
         return False
     return True
 
@@ -79,7 +93,7 @@ def in_stock_check(chromeDriver):
     inStock = False
 
     try:
-        if chromeDriver.find_element_by_id("priceblock_ourprice").text:
+        if chromeDriver.find_element_by_id("corePriceDisplay_desktop_feature_div").text: #("sns-base-price").text:
             l.info("Item is in-stock!")
             inStock = True
         else:
@@ -111,7 +125,7 @@ def seller_check(chromeDriver):
 
 def verify_price_within_limit(chromeDriver):
     try:
-        price = chromeDriver.find_element_by_id('priceblock_ourprice').text
+        price = re.sub(r"\D", "", chromeDriver.find_element("xpath", '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[2]').text)
     except Exception as e:
         l.error('Error verifying price: {}'.format(e))
         return False
@@ -130,28 +144,40 @@ def checkout(chromeDriver):
     l.info("Checking out...")
     chromeDriver.get(ITEM_URL)
 
-    chromeDriver.find_element_by_id('submit.add-to-cart').click()  # add to cart
-
-    chromeDriver.get(CHECKOUT_URL)
-
     try:
         WebDriverWait(chromeDriver, 3).until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@name='proceedToRetailCheckout']"))).click()
-    except Exception as e:
-        l.warn(f"Could not place order: {e}")
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='buy-now-button']"))).click() # "//input[@name='proceedToRetailCheckout']"))).click()
+        return True
 
-    count = 0
-    while (count < 3):
-        try:
-            WebDriverWait(chromeDriver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, "//input[@name='placeYourOrder1']"))).click()
-            l.info("Placed order!")
-            return True
-        except Exception as e:
-            count += 1
-            l.warn(f"Try: {count}, Could not place order: {e}")
+        
+        
+    except Exception as e:
+        l.warn(f"Could not add order: {e}")
     return False
 
+def go_to_order(chromeDriver):
+    l.info("proceed to order...")
+    chromeDriver.get(PAYMENT_URL)
+    try:
+        WebDriverWait(chromeDriver, 3).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='orderSummaryPrimaryActionBtn']/span/input"))).click()
+        time.sleep(1)
+        return True
+
+    except Exception as e:
+        l.warn(f"Could not place card select on order: {e}") 
+
+    return False
+
+def submit_order(chromeDriver):
+    chromeDriver.get(SUBMIT_URL)
+    try:
+        WebDriverWait(chromeDriver, 3).until(
+            
+            EC.element_to_be_clickable((By.XPATH, "//input[@name='placeYourOrder1']"))).click()
+    except Exception as e:
+        l.warn(f"Try: , Could not place order: {e}")
+        
 
 def checkout_1click(chromeDriver):
     l.info("Buy now with 1-click")
--- main.py
+++ main.py
@@ -3,28 +3,30 @@
 from logger import logger
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+import chromedriver_binary
 
 import amazonBot
 import sys
+import pdb
 
 def launch():
     d = DesiredCapabilities.CHROME
     d['loggingPrefs'] = {'performance': 'ALL'}
 
     opt = webdriver.ChromeOptions()
-    opt.add_argument('--disable-xss-auditor')
-    opt.add_argument('--disable-web-security')
-    opt.add_argument('--disable-dev-shm-usage')
-    opt.add_argument('--allow-running-insecure-content')
-    opt.add_argument('--remote-debugging-port=921')
-    opt.add_argument('--disable-webgl')
-    opt.add_argument('--disable-popup-blocking')
+    # opt.add_argument('--disable-xss-auditor')
+    # opt.add_argument('--disable-web-security')
+    # opt.add_argument('--disable-dev-shm-usage')
+    # opt.add_argument('--allow-running-insecure-content')
+    # opt.add_argument('--remote-debugging-port=921')
+    # opt.add_argument('--disable-webgl')
+    # opt.add_argument('--disable-popup-blocking')
 
-    opt.add_argument('--no-sandbox')
-    opt.add_argument('--disable-gpu')
-    opt.add_argument('--window-size=1920,1080')
+    # opt.add_argument('--no-sandbox')
+    # opt.add_argument('--disable-gpu')
+    # opt.add_argument('--window-size=1920,1080')
 
-    opt.add_argument('--headless')
+    # opt.add_argument('--headless')
 
     browser = webdriver.Chrome('chromedriver', options=opt, desired_capabilities=d)
     # opt.add_argument("--user-data-dir=selenium") # added this option to use cookies, you may need to perform initial login within Selenium
@@ -55,6 +57,7 @@ if __name__ == '__main__':
                 if amazonBot.purchase_item(b):
                     done = True
                     logger.info("Successfully purchased item!!!!")
+                    pdb.set_trace()
             except Exception as e:
                 logger.error('{}'.format(e))
                 logger.error('!!! Exiting App !!!')
